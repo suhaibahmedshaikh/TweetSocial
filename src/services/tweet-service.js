99ddcbf0c1f171d58c3a31567a2ba37eebc9ccf3
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-08-08 23:39:17 +0530

    added some tweet service

---
+++ src/services/tweet-service.js
@@ -0,0 +1,15 @@
+const { TweetRepository } = require("../repository/index");
+
+class TweetService {
+  constructor() {
+    this.tweetRepository = new TweetRepository();
+  }
+
+  async create(data) {
+    const content = data.content;
+    const tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extracts hashtags
+    tags = tags.map((tag) => tag.substring(1));
+    const tweet = await this.tweetRepository.postTweet(data);
+    return tweet;
+  }
+}
